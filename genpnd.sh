13fb84154f765c586aab9a2a14ec24e659003d5a
Ludger Krämer <dbluelle@blau-weissoedingen.de>
2013-06-21 22:42:45 +0200

    add pnd generation stuff

---
+++ genpnd.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cp ./etr ./pnd/
+./pnd_make.sh -c -d pnd -p extremetuxracer.pnd -x pnd/extremetuxracer.xml -i pnd/etracericon.png
---
+++ pnd/start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export HOME=`pwd`
+export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH
+./etr
+
